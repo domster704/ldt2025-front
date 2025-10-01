29f757a181b60233744b8d5067ace3d1246add3f
domster704 <domster704@mail.ru>
2025-10-01 23:57:08 +0300

    add color highlighting for figo and forecast fields

--- src/entities/ctg-history/ui/CTGHistoryParamsTable.tsx
+++ src/entities/ctg-history/ui/CTGHistoryParamsTable.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import * as style from "./CTGHistoryParamsTable.module.css";
 import {PARAM_GROUPS, PREDICTIONS} from "@entities/ctg-history/model/paramsConfig";
+import {ctgColors, CTGStatus} from "@shared/const/ctgColors";
+import {CTGHistory} from "@entities/ctg-history/model/types";
 
 type Mode = "single" | "compare";
 
 interface CTGHistoryParamsTableProps extends React.TableHTMLAttributes<HTMLTableElement> {
   mode: Mode;
-  data: any | any[];
+  data: CTGHistory | CTGHistory[];
 }
 
 function formatValue(value: unknown) {
@@ -15,13 +17,24 @@ function formatValue(value: unknown) {
   return String(value);
 }
 
+function getValue(ctg: CTGHistory, key: string) {
+  return (ctg as any)[key];
+}
+
+function getFIGOBg(value: CTGStatus): string | undefined {
+  if (!value) {
+    return;
+  }
+  return ctgColors[value];
+}
+
 const CTGHistoryParamsTable: React.FC<CTGHistoryParamsTableProps> = ({
                                                                        mode,
                                                                        data,
                                                                        ...props
                                                                      }) => {
   if (mode === "single") {
-    const ctg = data;
+    const ctg = data as CTGHistory;
 
     return (
       <table {...props} className={[
@@ -34,17 +47,26 @@ const CTGHistoryParamsTable: React.FC<CTGHistoryParamsTableProps> = ({
             {group.map((param, pi) => (
               <React.Fragment key={pi}>
                 <td>{param.label}</td>
-                <td>{formatValue(ctg[param.key])}</td>
+                <td style={{
+                  backgroundColor: param.key.includes("figo") || param.key.includes("forecast")
+                    ? getFIGOBg(getValue(ctg, param.key))
+                    : undefined
+                }}>
+                  {formatValue(formatValue(getValue(ctg, param.key)))}
+                </td>
               </React.Fragment>
             ))}
           </tr>
         ))}
         <tr>
           {PREDICTIONS.map((p, i) => (
-            <td key={i} className={style.table__predict} colSpan={2}>
+            <td key={i} className={style.table__predict} colSpan={2}
+                style={{
+                  backgroundColor: getFIGOBg(getValue(ctg, p.key))
+                }}>
               <div>
                 <p>{p.label}</p>
-                <p>{formatValue(ctg[p.key])}</p>
+                <p>{formatValue(formatValue(getValue(ctg, p.key)))}</p>
               </div>
             </td>
           ))}
@@ -57,7 +79,7 @@ const CTGHistoryParamsTable: React.FC<CTGHistoryParamsTableProps> = ({
   }
 
   if (mode === "compare") {
-    const [ctg1, ctg2] = data;
+    const [ctg1, ctg2] = data as CTGHistory[];
 
     const flatParams = [...PARAM_GROUPS.flat(), ...PREDICTIONS];
 
@@ -73,12 +95,12 @@ const CTGHistoryParamsTable: React.FC<CTGHistoryParamsTableProps> = ({
           <th>
             {ctg1.date?.toLocaleDateString?.() ?? "Исследование 1"}
             <br/>
-            38+2 нед
+            {ctg1.gestation ?? ""}
           </th>
           <th>
             {ctg2.date?.toLocaleDateString?.() ?? "Исследование 2"}
             <br/>
-            38+2 нед
+            {ctg2.gestation ?? ""}
           </th>
         </tr>
         </thead>
@@ -86,8 +108,20 @@ const CTGHistoryParamsTable: React.FC<CTGHistoryParamsTableProps> = ({
         {flatParams.map((param, idx) => (
           <tr key={idx}>
             <td>{param.label}</td>
-            <td>{formatValue(ctg1[param.key])}</td>
-            <td>{formatValue(ctg2[param.key])}</td>
+            <td style={{
+              backgroundColor: param.key.includes("figo") || param.key.includes("forecast")
+                ? getFIGOBg(getValue(ctg1, param.key))
+                : undefined
+            }}>
+              {formatValue(formatValue(getValue(ctg1, param.key)))}
+            </td>
+            <td style={{
+              backgroundColor: param.key.includes("figo") || param.key.includes("forecast")
+                ? getFIGOBg(getValue(ctg2, param.key))
+                : undefined
+            }}>
+              {formatValue(formatValue(getValue(ctg2, param.key)))}
+            </td>
           </tr>
         ))}
         </tbody>
